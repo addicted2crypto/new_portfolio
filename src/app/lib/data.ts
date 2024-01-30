115476b6d1122401c1c81eb32051443825c65f22
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-01-29 18:23:21 -0800

    animate header and list links

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -2,9 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+
 
 export const links = [
   {
@@ -66,21 +64,21 @@ export const projectsData = [
     description:
       "I worked as a full-stack developer on this local buisness for a few months. The owner has helped me get many clients though word of mouth",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    // imageUrl: corpcommentImg,
   },
   {
     title: "Degen launchpad",
     description:
       " I am the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    // imageUrl: "",
   },
   {
     title: "Code4rena",
     description:
       "A community bug bounty program where the best in the world compete for bounties from established protocols.",
     tags: ["React", "Next.js", "Solidity", "Rust", "Framer","SQL"],
-    imageUrl: wordanalyticsImg,
+    // add image for roles
   },
 ] as const;
 
