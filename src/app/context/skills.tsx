c71730ef8dfe97b7582831bec109969d6078fc4b
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-02-11 00:58:43 -0800

    viewport once true skills sect fix continuas loops

--- src/app/context/skills.tsx
+++ src/app/context/skills.tsx
@@ -38,7 +38,7 @@ export default function Skills() {
           initial="initial"
           whileInView="animate"
           viewport={{
-            once: false,
+            once: true,
           }}
           custom={index}
           >
