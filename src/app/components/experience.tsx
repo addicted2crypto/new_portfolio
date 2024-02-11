b593b48bea9368d4bb0d600f6f3bf11b887748ba
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-02-11 00:55:03 -0800

    image imports from react-icons

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -8,6 +8,8 @@ import {
   } from "react-vertical-timeline-component";
   import "react-vertical-timeline-component/style.min.css";
 import { experienceData } from '../lib/data';
+import { isMapIterator } from 'util/types';
+import { IconBase } from 'react-icons';
 
 export default function Experience() {
   return (
@@ -17,7 +19,21 @@ export default function Experience() {
         <VerticalTimeline>
             {experienceData.map((role, index) => (
                     <React.Fragment key={index}>
-                    <VerticalTimelineElement visible={true}>
+                    <VerticalTimelineElement 
+                        visible={true}
+                        contentStyle={{
+                            background: '#f3f4f6',
+                            boxShadow: 'unset',
+                            border: '1px solid #06060a/.1',
+                            textAlign: 'left',
+                            padding: '1.369rem 2rem',
+                        }}
+                        contentArrowStyle={{
+                            borderRight: "0.6875rem solid #06060a"
+                        }}
+                        date={role.date}
+                        icon={role.icon}
+                    >
                         <h3 className='font-semibold capitalize'>{role.title}</h3>
                         <p>{role.location}</p>
                         <p>{role.description}</p>
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,10 +1,10 @@
 
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
+import { GiComputing } from "react-icons/gi";
+import { GiOpenTreasureChest } from "react-icons/gi";
 
-import  easImg  from "/public/eas.png";
+import easImg from "/public/eas.png";
 import coincreteImg from '/public/coincrete.png';
 import cftmarketImg from '/public/cftmarket.png';
 
@@ -42,24 +42,24 @@ export const experienceData = [
     location: "Seattle",
     description:
       "I am the lead developer for experience as a service. The begining of something huge.",
-    icon: React.createElement(LuGraduationCap),
+    icon: React.createElement(GiComputing),
     date: "2020 - Present",
   },
   {
     title: "Front-End Developer",
     location: "Web3",
     description:
-      "I have created a few front end website's and dapp's in the web3 industry",
-    icon: React.createElement(CgWorkAlt),
+      "I have created multipe diffrent front ends for a few projects. Working with leading dapp's in the web3 industry",
+    icon: React.createElement(FaReact),
     date: "2021 - Present",
-    
+
   },
   {
     title: "Full-Stack Developer",
     location: "Web3",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
+      "I'm now a full-stack developer working as a freelancer. Security focused!🔐 Refrences needed to work together in web3.",
+    icon: React.createElement(GiOpenTreasureChest),
     date: "2020 - present",
   },
 ] as const;
@@ -69,25 +69,25 @@ export const projectsData = [
     title: "EAS",
     description:
       "I was brought on as the lead dev from word of mouth to experience as a service.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma","Rust"],
-    
-    
-    imageUrl:easImg,
-    
-    
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma", "Rust"],
+
+
+    imageUrl: easImg,
+
+
   },
   {
     title: "CoinCrete",
     description:
-      " I am the front-end developer. It has all the needed features of a web3 dapp with security at the forefront.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Solidity","JavaScript","SQL","MongoDB","Postgres"],
+      " I am the front-end developer. We are creating from scratch all the needed features of a web3 dapp with security at the forefront.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Solidity", "JavaScript", "SQL", "MongoDB", "Postgres"],
     imageUrl: coincreteImg,
   },
   {
     title: "CFT",
     description:
       "A new idea for amazing market pools and a interactive place that can be trusted.",
-    tags: ["React", "Next.js", "Solidity", "Rust", "Framer","SQL"],
+    tags: ["React", "Next.js", "Solidity", "Rust", "Framer", "SQL"],
     imageUrl: cftmarketImg,
   },
 ] as const;
@@ -95,7 +95,7 @@ export const projectsData = [
 
 export const skillsData = [
   "JavaScript",
-  
+
   "React",
   "Typescript",
   "Next.js",
