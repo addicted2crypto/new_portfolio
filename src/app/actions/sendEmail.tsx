ff3a281110325a40ef72b0ac1cdd4805afce9880
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-02-11 20:44:26 -0800

    resend brought in with api keys for auto email resp

---
+++ src/app/actions/sendEmail.tsx
@@ -0,0 +1,19 @@
+"use server";
+
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+    
+    console.log("We are runing on the server yooo");
+    console.log(formData.get('senderEmail'));
+    console.log(formData.get('senderMessage'));
+
+    resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'tundra2727@gmail.com',
+        subject: 'Message from contact form',
+        text: 'Thank you for reaching out.',
+    });
+};
