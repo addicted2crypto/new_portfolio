54140e4a61892e09b6cc1414aa5c47455b79e9bf
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-02-16 22:18:48 -0800

    dark mode class in dom/toggle dark

---
+++ src/app/components/toggle-darkmode.tsx
@@ -0,0 +1,57 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import { BsMoon, BsSun } from 'react-icons/bs';
+
+export type Theme = "light" | "dark";
+
+export default function Toggledarkmode() {
+  const [theme, setTheme] = useState<Theme>("light");
+  
+
+  //stores on clients localstorage to remember prefrence
+  const toggleTheme = () => {
+    if(theme === 'light') {
+        setTheme('dark');
+        window.localStorage.setItem('theme', 'dark');
+        document.documentElement.classList.add('dark');
+    } else {
+        setTheme('light');
+        window.localStorage.setItem("theme", 'light');
+        document.documentElement.classList.remove('dark');
+    }
+    
+  };
+
+
+  //useEffect runs a check of local storage-> use effect is good for reading outside react in this situation
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme')as Theme | null;
+    if (localTheme) {
+        setTheme(localTheme);
+
+        if(localTheme === "dark"){
+            document.documentElement.classList.add('dark');
+        }
+
+    } else if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        setTheme('dark');
+        document.documentElement.classList.add('dark');
+    }
+
+  }, []);
+
+    return (
+    <button className='fixed md:top-3 md:right-3 sm:top-0 sm:right-0 bg-[#c4c4c3] bg-opacity-85 backdrop-blur-[0.5rem] border border-[#fff] shadow-2xl rounded-full flex items-center justify-center hover:scale-110 active:scale-110 transition-all w-[2rem] h-[2rem]'
+     onClick={toggleTheme}
+    >
+        {
+            theme === 'light' ? (
+                <BsSun />
+            ) : (
+                <BsMoon />
+            )
+        }
+       
+    </button>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from './components/header';
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
 import Footer from './components/footer';
+import Toggledarkmode from './components/toggle-darkmode';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className}
-      bg-gray-50 text-gray-950 relative`}>
+      bg-gray-50 text-gray-950 relative dark:bg-[#100f0f] dark:text-[#d6d6d6] `}>
         <div className='bg-[#e7ffe1] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75] pt-28'>
         </div>
         <div className='bg-[#c1d7fe] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'>
@@ -34,6 +35,7 @@ export default function RootLayout({
 
         <Toaster position='top-center'/>
         <Footer />
+        <Toggledarkmode />
         </body>
     </html>
   );
