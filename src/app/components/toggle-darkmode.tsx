530c13a3b39c48a0efc5966aab455499f53db54f
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-02-18 00:57:58 -0800

    dark toggle to bottom left

--- src/app/components/toggle-darkmode.tsx
+++ src/app/components/toggle-darkmode.tsx
@@ -10,7 +10,7 @@ export default function Toggledarkmode() {
     const { theme, toggleTheme } = useDarkMode();
    
     return (
-    <button className='fixed md:top-3 md:right-3 sm:top-0 sm:right-0 bg-[#c4c4c3] bg-opacity-85 backdrop-blur-[0.5rem] border border-[#fff] shadow-2xl rounded-full flex items-center justify-center hover:scale-110 active:scale-110 transition-all w-[2rem] h-[2rem] dark:bg-[#262626]'
+    <button className='fixed md:bottom-8 md:left-8 sm:bottom-5 sm:left-5 lg:bottom-18 lg:bottom-18 bg-[#c4c4c3] bg-opacity-85 backdrop-blur-[0.5rem] border border-[#fff] shadow-2xl rounded-full flex items-center justify-center hover:scale-110 active:scale-110 transition-all w-[2rem] h-[2rem] dark:bg-[#262626]'
      onClick={toggleTheme}
     >
         {
