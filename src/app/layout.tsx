8c060e6cccbe1b83b0f285272e7f5dde0519a4a5
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-05-07 16:16:29 -0700

    resume updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({
     <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className}
       bg-gray-50 text-gray-950 relative dark:bg-[#100f0f] dark:text-[#d6d6d6] `}>
-        <div className='bg-[#e7ffe1] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75] pt-28 dark:bg-[#1d3d15]'>
+        <div className='bg-[#075738] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75] pt-28 dark:bg-[#1d3d15]'>
         </div>
-        <div className='bg-[#c1d7fe] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#5493ff]'>
+        <div className='bg-[#3377ee] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#071e45]'>
         </div>
 
         <DarkContextProvider>
