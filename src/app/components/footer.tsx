f49276bc666d44e601545ecf9f3306b715ff78d1
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-03-20 20:57:10 -0700

    2025

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
      <footer className='mb-10 px-4 text-center text-[#6f5151] absolute bottom-0'>
         <small className='mb-2 block text-xs'>
-        &copy; 2024. All rights reserved. We support all builds as an integral part of the developer team. Security is our top priority in development.
+        &copy; 2025. All rights reserved. We support all builds as an integral part of the developer team. Security is always our top priority.
         </small>
         <p className='text-xs'>
             <span className='font-semibold '>About my portfolio website:</span> built with React, Next.js, TypeScript, Tailwind, Framer Motion, React Email with Resend email support, Hosting on Vercel.
