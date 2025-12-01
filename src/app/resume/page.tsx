6cbbd1db6a5c3c158546363986ee947a3781e3ee
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-11-30 22:45:32 -0800

    Routing to new Res

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function ResumePage() {
   // Redirect to the static HTML resume in public folder
-  redirect('/resume.html');
+  redirect('/William_Heeb_Resume.html');
 }
